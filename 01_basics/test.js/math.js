2f7b947f661925e701c589d65f2ff4dccd2ffb32
Urvashi9776 <145040968+Urvashi9776@users.noreply.github.com>
2024-05-16 19:03:30 +0000

    basic1

---
+++ 01_basics/test.js/math.js
@@ -0,0 +1,13 @@
+/*const score=400
+const balance =new Number(200)
+console.log(balance.toString().length)
+console.log(balance.toFixed(3));
+
+const otherNum=123.456
+console.log(otherNum.toPrecision(4));
+const h=100000000
+console.log(h.toLocaleString())
+*/
+//----------------------------------------MATHS-----------------------------------
+
+console.log(Math);
