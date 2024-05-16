b7f5a9ed07333e8b17193c05185735f189dde01e
Urvashi9776 <145040968+Urvashi9776@users.noreply.github.com>
2024-05-16 19:03:07 +0000

    datatype

---
+++ 01_basics/test.js/datatype.js
@@ -0,0 +1,28 @@
+"use strict" //treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+let name="Uri"
+let age=14
+let isLoggedin=false
+let state;
+
+//number => 2 to power 53
+//bigInt
+//string=>""
+//boolean=>true/false
+//null=>standalone dat type
+//undefined
+//symbol=>unique
+//primitive data types
+
+//object
+
+console.log(typeof(null))        //object
+console.log(typeof(undefined))  //undefined
+
