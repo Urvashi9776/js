cfa7ad73b4216ff2ce274fd9e6e25cbc4d9631f6
Urvashi9776 <145040968+Urvashi9776@users.noreply.github.com>
2024-05-18 05:30:11 +0000

    iteration

---
+++ iterations/two.js
@@ -0,0 +1,20 @@
+let i=0;
+while(i<10)
+    {
+        console.log(i);
+        i++;
+    }
+
+    let myArr=["uri","puri","duri"]
+    let arr=0
+    while(arr<myArr.length)
+        {
+            console.log(myArr[arr]);
+            arr++;
+        }
+
+        let score=11
+        do {
+            console.log(score);
+            score++
+        } while (score<=10);
